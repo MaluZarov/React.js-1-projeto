e6281ab29fcfe02bf6f34c144659dae503bf7e3d
Malu Zarov Goncalves <90872311+MaluZarov@users.noreply.github.com>
2022-06-04 15:52:01 -0300

    meu_site_react

---
+++ meu_site_react/src/components/Hello.js
@@ -0,0 +1,35 @@
+function Hello() {
+
+    return(
+
+        
+           
+        
+
+        <div>
+            
+            
+            <img src="https://media-exp1.licdn.com/dms/image/C5616AQG5HtLQwV6Itg/profile-displaybackgroundimage-shrink_350_1400/0/1639779148075?e=1659571200&v=beta&t=8S49s7bQdqS3zwyabbT8lb18FyGX44PZq3ocn2jjLL8"></img>
+            <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEMG2VB_FWDag/profile-displayphoto-shrink_400_400/0/1649032637355?e=1659571200&v=beta&t=n-tjgIxVLjGdSLYmiu_oBUhCqXQlo9WzmneKfEWBBPw"></img> 
+            <img src="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/168570351/original/fe86a10c5f53958c74f92bb8befe4d0c3b6ee10c/do-html-css-javascript-ajax-reactjs-psd-html-jquery.png"></img>
+           
+           
+            
+            
+            <h1>
+                Malu Zarov Goncalves Lourenco
+            </h1>
+            <h2>21 Anos
+                
+            </h2>
+            <h3>Sistema da informacao - Faculdade descomplica</h3>
+            <h4>Desenvolvedora Fullstack - Transforma Tech(CARREFOUR-DIO)</h4>
+           
+            <img src="https://media-exp1.licdn.com/dms/image/C5616AQG5HtLQwV6Itg/profile-displaybackgroundimage-shrink_350_1400/0/1639779148075?e=1659571200&v=beta&t=8S49s7bQdqS3zwyabbT8lb18FyGX44PZq3ocn2jjLL8"></img>
+            <img src="https://media-exp1.licdn.com/dms/image/C5616AQG5HtLQwV6Itg/profile-displaybackgroundimage-shrink_350_1400/0/1639779148075?e=1659571200&v=beta&t=8S49s7bQdqS3zwyabbT8lb18FyGX44PZq3ocn2jjLL8"></img>
+
+        </div>
+    )
+}
+
+export default Hello
